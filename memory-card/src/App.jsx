4d3b21a13f3571e331ae460abfb3eea46c88d0b9
Christian Valeri <mortlaxris@outlook.com>
2023-08-17 20:36:59 -0500

    Add imgs, basic html pics and name

--- memory-card/src/App.jsx
+++ memory-card/src/App.jsx
@@ -1,14 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import { useEffect } from 'react';
 import './App.css'
+
+
+const storeCardInfo = [
+  {name: "charmander", img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png", value: 1},
+  {name: "bulbasaur", img: "https://archives.bulbagarden.net/media/upload/f/fb/0001Bulbasaur.png", value: 2},
+  {name: "squirtle", img: "https://assets.stickpng.com/images/580b57fcd9996e24bc43c32a.png", value: 3},
+  {name: "cyndaquil", img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/155.png", value: 4},
+  {name: "chikorita", img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/152.png", value: 5},
+  {name: "totodile", img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/158.png", value: 6},
+  {name: "trecko", img: "https://gamepress.gg/pokemongo/sites/pokemongo/files/2017-08/252_1.png", value: 7},
+  {name: "torchic", img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/255.png", value: 8},
+  {name: "mudkip", img: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/258.png", value: 9}
+];
+
+
+let shuffled;
+function randomizeArray() {
+  shuffled = storeCardInfo
+    .map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value)
+console.log(shuffled);
+}
+
+
+function CreateDiv() {
+  return (
+    <div>
+          {shuffled.map((shuffled) => (
+            <figure key={shuffled.value}>
+              <img src={shuffled.img} alt={shuffled.name}/>
+              <figcaption>{shuffled.name}</figcaption>
+            </figure>
+      ))}
+    </div>
+  )
+}
 
 function App() {
 
   return (
     <>
+      <CreateDiv />
     </>
   )
 }
 
-export default App
+export default App;
+
+window.onload = randomizeArray();
